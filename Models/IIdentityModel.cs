9e05aa0e4a08b18131d55b8df80e323e70d24373
Igi <brad@igi.codes>
2018-12-22 22:06:53 +0000

    Created interface for IdentityModel

--- Models/IIdentityModel.cs
+++ Models/IIdentityModel.cs
@@ -1,54 +1,34 @@
 ﻿using System;
-using System.ComponentModel.DataAnnotations;
-using JetBrains.Annotations;
-using NFive.SDK.Core.Helpers;
 
 namespace NFive.SDK.Core.Models
 {
-	/// <inheritdoc />
 	/// <summary>
 	/// Represents the key properties for a stored model.
 	/// </summary>
-	[PublicAPI]
-	public abstract class IdentityModel : IIdentityModel
+	public interface IIdentityModel
 	{
-		/// <inheritdoc />
 		/// <summary>
 		/// Gets or sets the model identifier.
 		/// </summary>
 		/// <value>
 		/// The model identifier.
 		/// </value>
-		[Key]
-		[Required]
-		public Guid Id { get; set; }
+		Guid Id { get; set; }
 
-		/// <inheritdoc />
 		/// <summary>
 		/// Gets or sets the timestamp of when the model was created.
 		/// </summary>
 		/// <value>
 		/// The timestamp of when the model was created.
 		/// </value>
-		[Required]
-		public DateTime Created { get; set; }
+		DateTime Created { get; set; }
 
-		/// <inheritdoc />
 		/// <summary>
 		/// Gets or sets the timestamp of when the model was deleted.
 		/// </summary>
 		/// <value>
 		/// The timestamp of when the model was deleted.
 		/// </value>
-		public DateTime? Deleted { get; set; }
-
-		/// <summary>
-		/// Initializes a new instance of the <see cref="IdentityModel"/> class.
-		/// </summary>
-		protected IdentityModel()
-		{
-			this.Id = GuidGenerator.GenerateTimeBasedGuid();
-			this.Created = DateTime.UtcNow;
-		}
+		DateTime? Deleted { get; set; }
 	}
 }
