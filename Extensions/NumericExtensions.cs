f9f614469dc94a500b603eebdf96e8a99ee1d4fe
Igi <brad@igi.codes>
2018-12-23 02:04:01 +0000

    Added postion and numeric extensions

---
+++ Extensions/NumericExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NFive.SDK.Core.Extensions
+{
+
+	public static class NumericExtensions
+	{
+		/// <summary>
+		/// Convert to Radians.
+		/// </summary>
+		/// <param name="val">The value to convert to radians</param>
+		/// <returns>The value in radians</returns>
+		public static double ToRadians(this float val)
+		{
+			return (Math.PI / 180) * val;
+		}
+
+		public static float Lerp(this float v0, float v1, float interval) => (1 - interval) * v0 + interval * v1;
+	}
+}
---
+++ Extensions/PositionExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using NFive.SDK.Core.Models;
+
+namespace NFive.SDK.Core.Extensions
+{
+	public static class PositionExtensions
+	{
+		public static Position TranslateDir(this Position pos, float angleInDegrees, float distance) =>
+			new Position(
+				pos.X + (float)Math.Cos(angleInDegrees.ToRadians()) * distance,
+				pos.Y + (float)Math.Sin(angleInDegrees.ToRadians()) * distance,
+				pos.Z
+			);
+
+		public static Position Lerp(Position pos1, Position pos2, float normalisedInterval) =>
+			new Position(
+				pos1.X.Lerp(pos2.X, normalisedInterval),
+				pos1.Y.Lerp(pos2.Y, normalisedInterval),
+				pos1.Z.Lerp(pos2.Z, normalisedInterval)
+			);
+
+		public static Position InFrontOf(this Position position, float heading, float distance) => position.TranslateDir(heading + 90, distance);
+	}
+}
