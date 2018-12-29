0da363910bd576c3a7fe6a2f7a3808d2f005c00b
Igi <brad@igi.codes>
2018-12-29 02:36:18 +0000

    Added ChatMessage

---
+++ Chat/ChatMessage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using JetBrains.Annotations;
+using NFive.SDK.Core.Helpers;
+using NFive.SDK.Core.Models.Player;
+
+namespace NFive.SDK.Core.Chat
+{
+	[PublicAPI]
+	public class ChatMessage : IChatMessage
+	{
+		public Guid Id { get; set; }
+		public User Sender { get; set; }
+		public string Content { get; set; }
+		[CanBeNull] public User Target { get; set; }
+
+		public ChatMessage()
+		{
+			this.Id = GuidGenerator.GenerateTimeBasedGuid();
+		}
+	}
+}
