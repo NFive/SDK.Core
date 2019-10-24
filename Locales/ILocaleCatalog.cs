635284b87f201c70994ca327538f1daf1c049c82
Joe Biellik <joebiellik@gmail.com>
2019-10-24 23:07:47 +0100

    Add method aliases

--- Locales/ILocaleCatalog.cs
+++ Locales/ILocaleCatalog.cs
@@ -10,18 +10,34 @@ namespace NFive.SDK.Core.Locales
 
 		string GetString(string text);
 
+		string _(string text);
+
 		string GetString(string text, params object[] args);
+
+		string _(string text, params object[] args);
 
 		string GetPluralString(string text, string pluralText, long n);
 
+		string _n(string text, string pluralText, long n);
+
 		string GetPluralString(string text, string pluralText, long n, params object[] args);
+
+		string _n(string text, string pluralText, long n, params object[] args);
 
 		string GetParticularString(string context, string text);
 
+		string _p(string context, string text);
+
 		string GetParticularString(string context, string text, params object[] args);
+
+		string _p(string context, string text, params object[] args);
 
 		string GetParticularPluralString(string context, string text, string pluralText, long n);
 
+		string _pn(string context, string text, string pluralText, long n);
+
 		string GetParticularPluralString(string context, string text, string pluralText, long n, params object[] args);
+
+		string _pn(string context, string text, string pluralText, long n, params object[] args);
 	}
 }
