36b4396460f84ca596add4d83cb6264080be783e
Joe Biellik <joebiellik@gmail.com>
2019-11-15 19:55:00 +0000

    Add serializer configuration

--- Utilities/Serializer.cs
+++ Utilities/Serializer.cs
@@ -6,16 +6,23 @@ namespace NFive.SDK.Core.Utilities
 	/// <summary>
 	/// Utilities to convert objects to and from JSON strings.
 	/// </summary>
-	/// TODO: Convert to ISerializer interface and move Newtonsoft.Json requirement to higher level.
 	[PublicAPI]
 	public class Serializer
 	{
+		/// <summary>Gets or sets the formatting option for the serializer.</summary>
+		/// <value>The formatting option.</value>
+		public Formatting Formatting { get; set; } = Formatting.None;
+
+		/// <summary>Gets or sets the settings for the serializer.</summary>
+		/// <value>The serializer settings.</value>
+		public JsonSerializerSettings Settings { get; set; }
+
 		/// <summary>
 		/// Serializes the specified object to JSON.
 		/// </summary>
 		/// <param name="obj">The object to serialize.</param>
 		/// <returns>The JSON representation of the object.</returns>
-		public string Serialize(object obj) => JsonConvert.SerializeObject(obj);
+		public string Serialize(object obj) => JsonConvert.SerializeObject(obj, this.Formatting, this.Settings);
 
 		/// <summary>
 		/// Deserializes the specified JSON to a type.
@@ -23,6 +30,6 @@ namespace NFive.SDK.Core.Utilities
 		/// <typeparam name="T">The type to deserialize the JSON as.</typeparam>
 		/// <param name="json">The JSON to deserialize.</param>
 		/// <returns>The deserialized object.</returns>
-		public T Deserialize<T>(string json) => JsonConvert.DeserializeObject<T>(json);
+		public T Deserialize<T>(string json) => JsonConvert.DeserializeObject<T>(json, this.Settings);
 	}
 }
