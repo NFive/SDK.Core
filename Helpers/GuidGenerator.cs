b6eb0cc5636686a93cb49fd2f00a6a5f57b08854
Thiago Zimmermann <13345912+thiago-dev@users.noreply.github.com>
2022-09-11 13:32:45 +0200

    Leave GuidGenerator unchanged.

--- Helpers/GuidGenerator.cs
+++ Helpers/GuidGenerator.cs
@@ -1,9 +1,14 @@
 using System;
-//using System.Security.Cryptography;
-
+using System.Security.Cryptography;
+using JetBrains.Annotations;
 
 namespace NFive.SDK.Core.Helpers
 {
+	/// <summary>
+	/// Utility for generating MySQL optimized globally unique identifiers.
+	/// </summary>
+	/// TODO: Fully test and verify this helps performance.
+	[PublicAPI]
 	public static class GuidGenerator
 	{
 		/// <summary>
@@ -12,28 +17,25 @@ namespace NFive.SDK.Core.Helpers
 		/// <returns>The generated GUID.</returns>
 		public static Guid GenerateTimeBasedGuid()
 		{
-			//var randomBytes = new byte[10];
+			var randomBytes = new byte[10];
+			using (var rng = new RNGCryptoServiceProvider()) rng.GetBytes(randomBytes);
 
-			//using (var rng = RandomNumberGenerator.Create()) rng.GetBytes(randomBytes);
+			var timestampBytes = BitConverter.GetBytes(DateTime.UtcNow.Ticks / 10000L);
+			if (BitConverter.IsLittleEndian) Array.Reverse(timestampBytes);
 
-			//var timestampBytes = BitConverter.GetBytes(DateTime.UtcNow.Ticks / 10000L);
-			//if (BitConverter.IsLittleEndian) Array.Reverse(timestampBytes);
+			var guidBytes = new byte[16];
 
-			//var guidBytes = new byte[16];
+			Buffer.BlockCopy(timestampBytes, 2, guidBytes, 0, 6);
+			Buffer.BlockCopy(randomBytes, 0, guidBytes, 6, 10);
 
-			//Buffer.BlockCopy(timestampBytes, 2, guidBytes, 0, 6);
-			//Buffer.BlockCopy(randomBytes, 0, guidBytes, 6, 10);
+			// ReSharper disable once InvertIf
+			if (BitConverter.IsLittleEndian)
+			{
+				Array.Reverse(guidBytes, 0, 4);
+				Array.Reverse(guidBytes, 4, 2);
+			}
 
-			//// ReSharper disable once InvertIf
-			//if (BitConverter.IsLittleEndian)
-			//{
-			//	Array.Reverse(guidBytes, 0, 4);
-			//	Array.Reverse(guidBytes, 4, 2);
-			//}
-
-			//return new Guid(guidBytes);
-
-			return Guid.NewGuid();
+			return new Guid(guidBytes);
 		}
 	}
 }
