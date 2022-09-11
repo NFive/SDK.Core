26d9cbacdb905830f22e5812109e9337c658d614
Thiago Zimmermann <13345912+thiago-dev@users.noreply.github.com>
2022-09-11 13:32:22 +0200

    Turn off auto-generated assembly attributes

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NFive Core SDK")]
+[assembly: AssemblyDescription("NFive core SDK for plugin development")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("NFive")]
+[assembly: AssemblyProduct("NFive SDK")]
+[assembly: AssemblyCopyright("Copyright © NFive 2018-2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("1ed39a02-e017-407d-8d32-fee8a1e29264")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0.0")]
